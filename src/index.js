95425b6d816d77ee7160584397713cfdc39c6566
Joshua Gonzalez <gonjosh@gmail.com>
2022-05-25 22:17:06 -0400

    created loop for image and dog buttons gave access to data selected elements that go into ui

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 const dataEndPoint = 'http://localhost:3000/pups'
 function getAllPups() {
-    fetch(dataEndPoint)
+   return fetch(dataEndPoint)
     .then(response => response.json());
 }
 
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const dogData =  getAllPups();
-  const dogBar = document.getElementById("dog-bar")
-  console.log(dogBar)
-});
-
-//<span>Mr. Bonkers</span>
-document.addEventListener('DOMContentLoaded', () => {
-  // selecting main content
-  const mainContent = document.querySelector('#dog-bar');
-  const dogData =  getAllPups();
-  // create element
+// create element
+function createDogButton(dogName, mainContent){
   const dogBar = document.createElement('span');
   dogBar.classList.add('dog-name');
-  dogBar.textContent = 'Mr.Bonkers';
+  dogBar.textContent = dogName;
+
+  mainContent.append(dogBar);
+}
+function createDogImage(dogName, mainContent){
+  const dogBar = document.createElement('img');
+  dogBar.setAttribute('src', dogName);
+
+  mainContent.append(dogBar);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // selecting elements that UI goes into
+  const mainContent = document.querySelector('#dog-bar');
+  const dogInfo = document.querySelector('#dog-info');
+
+  // its giving access to the data
+  const dogData = await getAllPups();
+
+// looping through the data creating dog button and creating image button
+  dogData.forEach(dog => {
+    createDogButton(dog.name, mainContent)
+    createDogImage(dog.image, dogInfo)
+  });
+
 
   // add to page
-  mainContent.append(dogBar);
+
 })

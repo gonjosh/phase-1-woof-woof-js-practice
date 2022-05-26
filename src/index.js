5e853bfda79ba71dff4d7a8c3f10dca657d4e054
Joshua Gonzalez <gonjosh@gmail.com>
2022-05-25 21:21:29 -0400

    put pups name in dog bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const dataEndPoint = 'http://localhost:3000/pups'
 function getAllPups() {
     fetch(dataEndPoint)
-    .then(response => response.json())
-    .then(data => dogMaker(data));
+    .then(response => response.json());
 }
-getAllPups()
 
-function dogMaker(allPups){
-    console.table(allPups)
-}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dogData =  getAllPups();
+  const dogBar = document.getElementById("dog-bar")
+  console.log(dogBar)
+});
+
+//<span>Mr. Bonkers</span>
+document.addEventListener('DOMContentLoaded', () => {
+  // selecting main content
+  const mainContent = document.querySelector('#dog-bar');
+  const dogData =  getAllPups();
+  // create element
+  const dogBar = document.createElement('span');
+  dogBar.classList.add('dog-name');
+  dogBar.textContent = 'Mr.Bonkers';
+
+  // add to page
+  mainContent.append(dogBar);
+})
